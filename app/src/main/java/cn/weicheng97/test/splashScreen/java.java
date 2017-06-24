9ffb71f08392f2cbfc432b4e7a022e7eec4843ad
Weicheng <williamjiang97@gmail.com>
2017-06-24 15:11:21 +0800

    创建

---
+++ app/src/main/java/cn/weicheng97/test/splashScreen/java.java
@@ -0,0 +1,20 @@
+package cn.weicheng97.test.splashScreen;
+
+        import android.app.Activity;
+        import android.content.Intent;
+        import android.content.pm.PackageInfo;
+        import android.content.pm.PackageManager;
+        import android.content.pm.PackageManager.NameNotFoundException;
+        import android.graphics.PixelFormat;
+        import android.os.Bundle;
+        import android.os.Handler;
+        import android.view.WindowManager;
+        import android.widget.TextView;
+
+/**
+ * Created by Weicheng on 2017/6/7.
+ */
+
+public class java {
+
+}
